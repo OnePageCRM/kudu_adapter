ef98669b76c9bc7b9a947f5454172f8ebd6ec4c5
Pawel Smolinski <pawel@onepagecrm.com>
2017-07-25 11:20:06 +0100

    Allow schema migration creation

--- lib/active_record/connection_adapters/kudu/database_statements.rb
+++ lib/active_record/connection_adapters/kudu/database_statements.rb
@@ -8,7 +8,16 @@ module ActiveRecord
       # :nodoc:
       module DatabaseStatements
         # :nodoc:
-        def exec_query(sql, name = 'SQL', binds = [], prepare: false)
+        def exec_query(sql, _ = 'SQL', binds = [], prepare: false)
+          ::Rails.logger.warn 'Prepared statements are not supported' if prepare
+
+          unless without_prepared_statement? binds
+            type_casted_binds(binds).each do |bind|
+              bind = "'#{bind}'" if bind.is_a? ::String # TODO: proper escape
+              sql = sql.sub('?', bind.to_s)
+            end
+          end
+
           result = connection.query sql
           columns = result.first&.keys.to_a
           rows = result.map { |row| row.fetch_values(*columns) }
